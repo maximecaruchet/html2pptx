5f14117da4f0c5e36b60b50fd98ac0a5a1990812
Maxime Caruchet <caruchet.maxime@gmail.com>
2019-06-01 13:24:11 +0200

    refactor: clean up code, refactor variable names and add comments

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ SLIDE_BLANK_LAYOUT = 6
 SLIDE_WIDTH_INCHES = 10
 SLIDE_HEIGHT_INCHES = 7.5
 SLIDE_SMALL_MARGIN_INCHES = 0.25
+COLUMN_MARGIN_INCHES = 0.1
+HEIGHT_MARGIN_INCHES = 0.1
 
-debug_logs = False
-debug_slide = False
+debug_logs = True
+debug_slide = True
 
 
 def html_to_pptx(url, css_selector):
@@ -103,8 +105,11 @@ def slides_to_pptx(slides):
 
 
 def fill_slide(prs, slide):
+    # Init default count values
     image_count = 0
     max_chars_in_strings = 0
+
+    # Determine number of images and max string length
     for slide_data in slide:
         img_found = re.search("^img_src:(.*)$", slide_data)
         if img_found:
@@ -112,153 +117,189 @@ def fill_slide(prs, slide):
         else:
             if len(slide_data) > max_chars_in_strings:
                 max_chars_in_strings = len(slide_data)
+
+    # Determine if the slide is empty (no images and, max string length = 0, wich means no images and no text)
     empty_slide = image_count == 0 and max_chars_in_strings == 0
     if empty_slide:
         return
 
+    # Add a slide when not empty
     prs_slide_layout = prs.slide_layouts[SLIDE_BLANK_LAYOUT]
     prs_slide = prs.slides.add_slide(prs_slide_layout)
 
+    # Determine if we are in a column layout or not
+    # We have a column layout if we have more than 1 image, at least 1 text block
+    # and if the longest text is < SHORT_TEXT_LIMIT_CHARS chars
     with_multiple_images = image_count > 1
     with_short_texts = max_chars_in_strings != 0 and max_chars_in_strings <= SHORT_TEXT_LIMIT_CHARS
     column_layout = with_multiple_images and with_short_texts
 
+    # Init base data
     images_array = []
     text_array = []
     column_text_array = []
 
+    # Parse slide and separate images from text
     for slide_data in slide:
-        img_found = re.search("^img_src:(.*)$", slide_data)
+        image_found = re.search("^img_src:(.*)$", slide_data)
         if column_layout:
-            if img_found:
+            # Special handling of text if we are in a column layout
+            if image_found:
+                # Be sure to always associate an image with some text below
                 if len(images_array) == 0 and len(column_text_array) > 0:
+                    # If text has been found without an image at the beginning, add an "empty" placeholder image
                     images_array.append("empty")
                     text_array.append(column_text_array)
                     column_text_array = []
                 elif len(images_array) > 0:
+                    # When some images have already been found, just add associated text
+                    # to the previous image (empty or not)
                     text_array.append(column_text_array)
                     column_text_array = []
-                images_array.append(img_found.group(1))
+                images_array.append(image_found.group(1))
             else:
+                # Add text to the current column
                 column_text_array.append(slide_data)
         else:
-            if img_found:
-                images_array.append(img_found.group(1))
+            # Default handling when not in column layout
+            if image_found:
+                images_array.append(image_found.group(1))
             else:
                 column_text_array.append(slide_data)
 
     if column_layout:
+        # Always add the final text (empty or not) in column layout
         text_array.append(column_text_array)
     else:
+        # Add text only if some text has been found (there will only be one big column in this case)
         if len(column_text_array) > 0:
             text_array.append(column_text_array)
 
-    column_margin_inches = 0.1
-    height_margin_inches = 0.1
+    # Determine available space in slide and column layout
     available_slide_width_inches = SLIDE_WIDTH_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES \
-        - (len(images_array)-1)*column_margin_inches
+        - (len(images_array)-1)*COLUMN_MARGIN_INCHES
     column_width_inches = available_slide_width_inches
     if len(images_array) > 0:
         column_width_inches = available_slide_width_inches/len(images_array)
+
+    # Init default values to compute image heights
     max_image_height_inches = 0
     images_heights_inches = []
 
-    for index, img_link in enumerate(images_array):
+    for index, image_link in enumerate(images_array):
         if debug_logs:
-            print("IMAGE LINK:", img_link)
-        if img_link == "empty":
+            print("IMAGE LINK:", image_link)
+
+        if image_link == "empty":
+            # If we have an "empty" placeholder image, ignore it but add a height of "0" to the image heights array
             images_heights_inches.append(0)
             continue
 
+        # Determine image position
         top = Inches(SLIDE_SMALL_MARGIN_INCHES)
         left = Inches(SLIDE_SMALL_MARGIN_INCHES + index*column_width_inches
-                      + index*column_margin_inches)
+                      + index*COLUMN_MARGIN_INCHES)
 
-        image_req = requests.get(img_link)
-        img_bytes = io.BytesIO(image_req.content)
-        img_box = prs_slide.shapes.add_picture(img_bytes, left, top)
+        # Download image and add it to the slide
+        image_req = requests.get(image_link)
+        image_bytes = io.BytesIO(image_req.content)
+        image_box = prs_slide.shapes.add_picture(image_bytes, left, top)
 
-        ratio = img_box.width.inches / img_box.height.inches
-        if img_box.width.inches > SLIDE_WIDTH_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES:
-            img_box.width = Inches(SLIDE_WIDTH_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES)
-            img_box.height = Inches(img_box.width.inches / ratio)
-        if img_box.height.inches > SLIDE_HEIGHT_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES:
-            img_box.height = Inches(SLIDE_HEIGHT_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES)
-            img_box.width = Inches(img_box.height.inches * ratio)
+        # Determine image ratio and resize image (with aspect ratio preserved) to fit it in the slide if it is too large
+        ratio = image_box.width.inches / image_box.height.inches
+        if image_box.width.inches > SLIDE_WIDTH_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES:
+            image_box.width = Inches(SLIDE_WIDTH_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES)
+            image_box.height = Inches(image_box.width.inches / ratio)
+        if image_box.height.inches > SLIDE_HEIGHT_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES:
+            image_box.height = Inches(SLIDE_HEIGHT_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES)
+            image_box.width = Inches(image_box.height.inches * ratio)
 
+        # Center image horizontally if only one image is found
         if len(images_array) == 1:
-            horizontal_image_center_inches = img_box.width.inches / 2
+            horizontal_image_center_inches = image_box.width.inches / 2
             slide_horizontal_center_inches = SLIDE_WIDTH_INCHES / 2
             left_horizontal_centered_inches = slide_horizontal_center_inches - horizontal_image_center_inches
             if left_horizontal_centered_inches < SLIDE_SMALL_MARGIN_INCHES:
                 left_horizontal_centered_inches = SLIDE_SMALL_MARGIN_INCHES
-            img_box.left = Inches(left_horizontal_centered_inches)
+            image_box.left = Inches(left_horizontal_centered_inches)
+
+            # Center image vertically if this one image is alone with no text column
             if len(text_array) == 0:
-                vertical_image_center_inches = img_box.height.inches / 2
+                vertical_image_center_inches = image_box.height.inches / 2
                 slide_vertical_center_inches = SLIDE_HEIGHT_INCHES / 2
                 top_vertical_centered_inches = slide_vertical_center_inches - vertical_image_center_inches
                 if top_vertical_centered_inches < SLIDE_SMALL_MARGIN_INCHES:
                     top_vertical_centered_inches = SLIDE_SMALL_MARGIN_INCHES
-                img_box.top = Inches(top_vertical_centered_inches)
+                image_box.top = Inches(top_vertical_centered_inches)
 
-        images_heights_inches.append(img_box.height.inches)
-        if img_box.height.inches > max_image_height_inches:
-            max_image_height_inches = img_box.height.inches
+        images_heights_inches.append(image_box.height.inches)
+        if image_box.height.inches > max_image_height_inches:
+            max_image_height_inches = image_box.height.inches
 
     for index, text_column in enumerate(text_array):
+        # For every text column, init default position and size
         left = Inches(SLIDE_SMALL_MARGIN_INCHES)
         width = Inches(SLIDE_WIDTH_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES)
         top = Inches(SLIDE_SMALL_MARGIN_INCHES)
         height = Inches(SLIDE_HEIGHT_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES)
 
         if len(images_array) > 0:
-            # override some default values if we have images
-            top = Inches(SLIDE_SMALL_MARGIN_INCHES + max_image_height_inches + height_margin_inches)
+            # Override some default values if we have images
+            top = Inches(SLIDE_SMALL_MARGIN_INCHES + max_image_height_inches + HEIGHT_MARGIN_INCHES)
             height = Inches(SLIDE_HEIGHT_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES
-                            - max_image_height_inches - height_margin_inches)
+                            - max_image_height_inches - HEIGHT_MARGIN_INCHES)
 
         if column_layout:
             # Column layout gets the final override if enabled
             left = Inches(SLIDE_SMALL_MARGIN_INCHES + index*column_width_inches
-                          + index*column_margin_inches)
+                          + index*COLUMN_MARGIN_INCHES)
             width = Inches(column_width_inches)
-            top = Inches(SLIDE_SMALL_MARGIN_INCHES + images_heights_inches[index] + height_margin_inches)
+            top = Inches(SLIDE_SMALL_MARGIN_INCHES + images_heights_inches[index] + HEIGHT_MARGIN_INCHES)
             height = Inches(SLIDE_HEIGHT_INCHES - 2*SLIDE_SMALL_MARGIN_INCHES
-                            - images_heights_inches[index] - height_margin_inches)
+                            - images_heights_inches[index] - HEIGHT_MARGIN_INCHES)
 
-        tx_box = prs_slide.shapes.add_textbox(left, top, width, height)
+        # Create the text box
+        text_box = prs_slide.shapes.add_textbox(left, top, width, height)
 
         if debug_slide:
-            fill = tx_box.fill
+            # Fill the shape with red in debug mode
+            fill = text_box.fill
             fill.solid()
             fill.fore_color.rgb = RGBColor(255, 0, 0)
 
-        text_frame = tx_box.text_frame
+        # Create the text frame inside the text box and configure it
+        text_frame = text_box.text_frame
         text_frame.clear()
         text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.MIDDLE
         if column_layout:
+            # Change vertical anchor in column layout mode
             text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.TOP
         text_frame.word_wrap = True
         text_frame.auto_size = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
 
+        # Add every string in the column to the text frame
         for text in text_column:
+            # Determine if the text is a title (no images + only this text alone)
             is_title = len(images_array) == 0 and len(text_array) == 1 and len(text_column) == 1
+
             if debug_logs:
                 print(text)
-            p = text_frame.paragraphs[0]
-            if p.text == "":
+
+            # Fill the text frame
+            paragraph = text_frame.paragraphs[0]
+            if paragraph.text == "":
                 if is_title:
-                    # title-like if only one text
-                    p.alignment = PP_PARAGRAPH_ALIGNMENT.CENTER
-                    p.font.size = Pt(TITLE_FONT_PT)
-                p.text = text
+                    # title format
+                    paragraph.alignment = PP_PARAGRAPH_ALIGNMENT.CENTER
+                    paragraph.font.size = Pt(TITLE_FONT_PT)
+                paragraph.text = text
             else:
-                p = text_frame.add_paragraph()
+                paragraph = text_frame.add_paragraph()
                 if is_title:
-                    # title-like if only one text
-                    p.alignment = PP_PARAGRAPH_ALIGNMENT.CENTER
-                    p.font.size = Pt(TITLE_FONT_PT)
-                p.text = text
+                    # title format
+                    paragraph.alignment = PP_PARAGRAPH_ALIGNMENT.CENTER
+                    paragraph.font.size = Pt(TITLE_FONT_PT)
+                paragraph.text = text
 
 
 # html_to_pptx("", "")
